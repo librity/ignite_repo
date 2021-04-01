a423bd6483e14d18151978397eb392817d2af8b0
Luis Geniole <luis.geniole@gmail.com>
2021-04-01 13:54:53 -0300

    Add fallback controller.

--- lib/repo_web/controllers/repos_controller.ex
+++ lib/repo_web/controllers/repos_controller.ex
@@ -1,11 +1,13 @@
 defmodule RepoWeb.ReposController do
   use RepoWeb, :controller
 
-  alias Github.Client, as: Repos
   alias Github.RepoInfo
+  alias RepoWeb.FallbackController
+
+  action_fallback FallbackController
 
   def show(conn, %{"username" => username}) do
-    with {:ok, [%RepoInfo{} | _tails] = repos} <- Repos.get_user_repos(username) do
+    with {:ok, [%RepoInfo{} | _tails] = repos} <- Repo.get_user_repos(username) do
       conn
       |> put_status(:ok)
       |> render("repos.json", repos: repos)
